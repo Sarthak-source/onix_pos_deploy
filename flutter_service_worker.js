f75a37c80f66570695e67aa004c1365c8577c982
Sarthak <sarthak.cmk.al@gmail.com>
2024-10-15 19:23:08 +0530

    quatity fix

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "1d0cf688f4491bb4caf7bf28ec38ffba",
+const RESOURCES = {"flutter_bootstrap.js": "4fe3af35b308f5cd07846b932c77a6ba",
 "version.json": "81c36c3acfb0510bde45f943605eeb2b",
 "index.html": "6b967cd3fbf27cf0444a245115b18bae",
 "/": "6b967cd3fbf27cf0444a245115b18bae",
-"main.dart.js": "f582353f8ae4958a7bdeb48f9a0e70d5",
+"main.dart.js": "0c5e510dee6880382a422681ec6f7c1e",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
